159fbb3c4efd3688250d23f5266b6885c3bb028c
guilherme-ro <guilherme.ramos09@gmail.com>
2020-12-29 14:00:51 -0300

    updated search box font-size

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba f7 b--green bg-lightest-blue'
         type='search'
         placeholder='Digite o nome de um Robô'
         onChange={searchChange}
